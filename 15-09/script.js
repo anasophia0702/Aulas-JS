bd1bd8528d1c5aacbff02f1cd813c006870c12a3
Ana Sophia <anasophia@gmail.com>
2026-09-15 11:38:45 -0300

    adicionada pasta 15-09 - variaveis

---
+++ 15-09/script.js
@@ -0,0 +1,42 @@
+// let miranha = "Peter Parker";
+// let numero = 99999999999;
+// let numero2 = "99999999999";
+// let idade = 17;
+//var nome = "Anna";
+// nome = "Sophia";
+
+//const nome = "Anna";
+// nome = "Sophia";
+
+// idade = 20;
+// idade= 21;
+
+// console.log("O miranha é o " + miranha + "e ele tem " + idade + " anos" );
+// Crie 4 variaveis contendo: nome, sobrenome, idade e curso 
+// Exiba no console em uma unica frase
+
+let nome = "Ana Sophia ";
+let sobrenome = "Oliveira Lins";
+let idade = " 17";
+let curso = " JavaScript";
+
+console.log("Eu sou a " + nome + sobrenome + ", tenho" + idade + " e estou fazendo o curso de" + curso + ".");
+
+
+// let nome = "Anna"; //string
+// let peso = 70 //int
+// let altura = 1.70; //float
+// let funcionario = false;//bool - true == verdadeiro ou false == falso
+
+// let cidade = null;
+// let estado;
+// console.log(typeof nome);
+// console.log(typeof peso);
+// console.log(typeof altura);
+// console.log(typeof funcionario);
+
+console.log("A variavel nome tem " + nome + "e é do tipo " + typeof nome)
+console.log("A variavel sobrenome tem " + sobrenome + " e é do tipo " + typeof sobrenome)
+console.log("A variavel idade tem " + idade + " e é do tipo " + typeof idade)
+console.log("A variavel curso tem " + curso + " e é do tipo " + typeof curso)
+
