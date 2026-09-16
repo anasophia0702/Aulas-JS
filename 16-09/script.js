e388cb39d307fe0d8c3d5776a40ec77482e258cf
Ana Sophia <anasophia@gmail.com>
2026-09-16 11:56:35 -0300

    criação da pasta 16-09 estrutura condicional

---
+++ 16-09/script.js
@@ -0,0 +1,149 @@
+// operadores aritimeticos
+// + soma
+// - subtração
+// * multiplicação
+// / divisão 
+// % sobra da divisão 
+
+// let n1 = 3;
+// let n2 = 9;
+
+// console.log(n1 + n2);
+// console.log(n1 - n2);
+// console.log(n1 * n2);
+// console.log(n2 / n1);
+// console.log(n2 % n1);
+// console.log((n2 - n1) * 2);
+// console.log("palavra" + n1);
+
+//Crie 3 variaveis, cada uma contendo um numero,
+// some as 3 e tire a media, exiba o resultado no console
+// A media de X X X é R
+
+// let n1 = 9
+// let n2 = 5 
+// let n3 = 7
+
+// console.log((n1 + n2 + n3) / 3);
+
+// let n1 = 9;
+// let n2 = 5;
+// let n3 = 7;
+
+
+// console.log(" A média de " + n1 + ",", n2 + ",", n3 + ","  + " é " + ( n1 + n2 + n3 ) / 3   );
+
+// > maior que 
+// < menor que
+// == igual 
+// >== maior ou igual 
+// >== menor ou igual 
+// != é diferente
+
+
+// let idade = 18;
+
+// console.log(idade > 18);
+// console.log(idade < 18);
+// console.log(idade == 18);
+// console.log(idade >= 18);
+// console.log(idade <= 18);
+// console.log(idade != 18);
+// console.log(idade === "18"); // comparação de tipo de variaveis
+
+// let idade = 18;
+// let habilitado = true;
+// console.log(idade <= 18 && habilitado == true);
+// V && V = V 
+// V && F = F
+// F && V = F
+// F && F = F
+
+// console.log(idade <= 18 || habilitado == true);
+// V || V = V 
+// V || F = V
+// F || V = V
+// F || F = F
+
+// ! operador NOT "inverte"
+// console.log(!habilitado);
+
+
+//se idade for igual a 18 
+    //faça isso
+
+// se nao 
+  // faça isso
+// let habilitado = false;
+// if (habilitado == true) {//se sim
+//     console.log("Pode dirigir");
+
+// } else {//se não
+//     console.log("NÃO pode dirigir");
+// }
+
+//  let idade = 16;
+
+// if (idade >= 18)
+// if (idade >= 17)
+//     console.log("Maior de idade!");
+
+//  else {
+//     console.log("Menor de idade");
+// }
+
+
+ //Crie um sistema que compare o valor de uma variavel 
+ //senha SE igual a 1234 escreva no console
+ //"acesso liberado" SE NÃO escreva "acesso bloqueado"
+
+//  let senha = 8374;
+
+//  if (senha == 1234 ) {
+//     console.log("ACESSO LIBERADO")
+
+//  } else {
+//     console.log("ACESSO BLOQUEADO")
+    
+//  }
+
+// Crie um sistema que avalie uma variavel nota
+// SE nota for maior que 6 escreva aprovado, SE NÃO 
+//escreva reprovado
+
+
+// let nota =  5;
+
+// if (nota >= 6) {
+//     console.log("APROVADO")
+// } else {
+//     console.log("REPROVADO")
+// }
+
+//Ex 3
+//Crie um sistema que compara um valor total de uma compra
+//SE o valor for maior que 500 calcule um desconto de 20 e mostre o valor total da compra
+//com desconto, SE NÃO mostre a mensagem
+//COMPRA SEM DESCONTO
+
+let valor = 350;
+let desconto = 20 ;
+
+if (valor >=500) {
+    console.log(valor - 20)
+} else {
+    console.log("COMPRA SEM DESCONTO")
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
